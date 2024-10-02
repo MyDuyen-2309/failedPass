15a10ee57969f09cef01ee65b71234f01d268cfb
MyDuyen-2309 <duyenttmd938@gmail.com>
2024-10-02 14:14:39 +0700

    failed

---
+++ src/test/java/failedPASS/CalculatorTest.java
@@ -0,0 +1,25 @@
+package failedPASS;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+	
+	private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddition() {
+        Assert.assertEquals(calculator.add(2, 3), 5); // Test case pass
+    }
+
+    @Test
+    public void testSubtraction() {
+        Assert.assertEquals(calculator.subtract(5, 3), 3); // Test case pass
+    }
+
+    @Test
+    public void testAdditionFail() {
+        Assert.assertEquals(calculator.add(2, 2), 5); // Test case fail
+    }
+
+}
